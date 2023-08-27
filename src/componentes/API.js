db9ec1eaed4e36d6ca36e78721a75cbd20cbca9d
SteveJobssss <SteveJobssss>
2023-08-27 18:03:53 -0500

    Correcion API

--- src/componentes/API.js
+++ src/componentes/API.js
@@ -28,7 +28,7 @@ function LocationMap() {
         <div className="location-map">
             <h1>Ubicación de Google Maps</h1>
             {location ? (
-                <LoadScript googleMapsApiKey="YOUR_API_KEY">
+                <LoadScript googleMapsApiKey="https://www.google.com/maps/place/Bucarica+Sector+18/@7.0674809,-73.0848064,17z/data=!3m1!4b1!4m6!3m5!1s0x8e684191e17600e7:0x56b2d9d27779e3fc!8m2!3d7.0674756!4d-73.0822261!16s%2Fg%2F11n0z6r6rh?entry=ttu">
                     <GoogleMap
                         mapContainerStyle={{ width: "400px", height: "400px" }}
                         center={{ lat: location.latitude, lng: location.longitude }}
