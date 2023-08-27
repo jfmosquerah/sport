d8b331859c21510895e387d9937c10f40e3d6852
jfmosquerah <jfmosquera@gmail.com>
2023-08-27 12:58:27 -0500

    Creacion de estilos de los componentes

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ProductosLista = () => {
     return (
         <div>
-            <h1>Productos</h1>
+            <h1>Productos lista</h1>
         </div>
     )
 }
