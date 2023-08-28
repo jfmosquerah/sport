7f0e7078a7a9c82943ade5f7ac10b860de7175c9
CesarRamirez <cesar.ramirez.8127@miremington.edu.co>
2023-08-27 21:43:50 -0500

    Limpieza de test

--- src/componentes/Header/header.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import index from './index';
-
-test('renders learn react link', () => {
-  render(<index />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
