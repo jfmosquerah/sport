3820cae3ce2ae7d36804a2dc221a7e79750c82d5
jfmosquerah <jfmosquera@gmail.com>
2023-08-29 18:46:11 -0500

    Cambio de carpeta de imagenes

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -1,30 +1,244 @@
 import React from 'react'
-import IMG from "../../images/img01.jpg"
+//import IMG from "../../images/img01.jpg"
 
 export const ProductosLista = () => {
     return (
       <>
-      <h1 className="title">PRODUCTOS</h1>
-        <div className="productos">
+        <h1 className="title">PRODUCTOS</h1>
           <div className="productos">
-          <a href="#">
-          <div className="producto__img">
-            <img src={IMG} alt=""/>
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>           
+          
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+            
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+            
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+            
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+            
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+            
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+            
+            <div className="producto">
+                <a href="#">
+                  <div className="producto__img">
+                    <img src="../../images/img01.jpg" alt=""/>
+                  </div>
+                </a>
+                <div className="producto__footer">
+                  <h1>Title</h1>
+                  <p>Categoria</p>
+                  <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                  <div>
+                    <a href="#" className="btn">
+                      Vista
+                    </a>
+                  </div>
+                </div>
+            </div>
+
           </div>
-          </a>
-          <div className="producto__footer">
-            <h1>Title</h1>
-            <p>Categoria</p>
-            <p className="price">$320</p>
-          </div>
-          <div className="buttom">
-            <button className="btn">
-              Añadir al carrito
-            </button>
-              <a href="#" className="btn">Vista</a>
-          </div>
-        </div>
-        </div>
+          
       </>
     )
 }
